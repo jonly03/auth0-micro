b38257748e460cd574c83102cfdf3af0d8ebc9c4
Nelly Sugu <jonly03@gmail.com>
2021-12-13 18:09:10 -0500

    init

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports.auth0_config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+};
---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const { auth } = require("express-openid-connect");
+const { auth0_config } = require("./config");
+const cors = require("cors");
+
+const server = express();
+server.use(auth(auth0_config));
+server.use(cors());
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on PORT: ${PORT}`);
+});
+
+server.get("/myLogin", (req, res) => {
+  res.redirect(
+    "https://dev-v8yhc0o7.us.auth0.com/authorize?response_type=token&client_id=tM4rxIGpbjNLFjuWxNcojBsOCy1hIQzP&redirect_uri=http://localhost:3000"
+  );
+});
+
+server.get("/", (req, res) => {
+  if (req.oidc.isAuthenticated()) {
+    return res.json({ user: req.oidc.user });
+  }
+
+  res.redirect("/myLogin");
+});
+
+// server.get("/", (req, res) => {
+//   if (req.oidc.isAuthenticated()) {
+//     console.log(req);
+//     return res.sendFile(__dirname + "/logged-in.html");
+//   }
+
+//   return res.sendFile(__dirname + "/logged-out.html");
+// });
