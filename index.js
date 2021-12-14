aafa150b036ac42bf14e0334b6e088ece5fcf13f
Nelly Sugu <jonly03@gmail.com>
2021-12-13 23:03:53 -0500

    fix api call

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { auth } = require("express-openid-connect");
 const { auth0_config } = require("./config");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const server = express();
 server.use(auth(auth0_config));
@@ -14,41 +15,22 @@ server.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
 });
 
-server.get("/myLogin", (req, res) => {
-  res.redirect(
-    "https://dev-v8yhc0o7.us.auth0.com/authorize?response_type=token&client_id=tM4rxIGpbjNLFjuWxNcojBsOCy1hIQzP&redirect_uri=https://www.nellysugu.com/auth_front/"
+server.get("/userinfo", async (req, res) => {
+  const { accessToken } = req.query;
+  if (!accessToken) {
+    return res
+      .status(400)
+      .json({ error: "accessToken query parameter missing" });
+  }
+
+  const { data: user } = await axios.get(
+    `${process.env.AUTH0_ISSUER_BASE_URL}/userinfo`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
   );
+
+  res.json({ user });
 });
-
-server.get("/", (req, res) => {
-  console.log(req.oidc.user);
-  if (req.oidc.isAuthenticated()) {
-    return res.json({ user: req.oidc.user });
-  }
-
-  res.redirect("/myLogin");
-});
-
-server.get("/callback", (req, res) => {
-  res.send("<p>test</p>");
-});
-
-server.get("/loggedIn", (req, res) => {
-  if (req.oidc.isAuthenticated()) {
-    return res.json({
-      isAuthenticated: true,
-      user: req.oidc.user,
-    });
-  }
-
-  return res.json({ isAuthenticated: false });
-});
-
-// server.get("/", (req, res) => {
-//   if (req.oidc.isAuthenticated()) {
-//     console.log(req);
-//     return res.sendFile(__dirname + "/logged-in.html");
-//   }
-
-//   return res.sendFile(__dirname + "/logged-out.html");
-// });
